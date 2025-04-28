67dd0b340be98d57c9e0a642faf3fca0bffa055d
B0maAp1r1 <337405@edumkcollege.ac.uk>
2025-04-28 13:04:57 +0100

    comit

--- feedback.php
+++ feedback.php
@@ -3,7 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="icon" href="./logos/Logo maker project.png">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="stylesheet.css">
+    <title>Feednack</title>
 </head>
 <body>
     <!-- navbar section -->
@@ -22,16 +26,17 @@
         </nav>
     </div>  
 
-    <div style="height: 100px;"></div>
+    <div style="height: 250px;"></div>
 
-    <div style="color: black; background: #75fb87; height: 500px; border-radius: 15px; box-shadow: rgba(100, 100, 111, 0.5) 0px 7px 29px 0px;">
-        <h1>
+    <div style="color: black; justify-self: center; width: 65%; background: #75fb87; height: 500px; border-radius: 15px; box-shadow: rgba(100, 100, 111, 0.5) 0px 7px 29px 0px;">
+        <h1 style="font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; Justify-self: center; position: relative; top: 75px;">
             Are you a Technical User or Non-Technical User
         </h1>
 
-        <div style="justify-self: center; justify-content: center;">
-            <button style="width: 150px; position: relative; border-radius: 10px; top: 5%; left: 15%;">Yes</button>
-            <button style="width: 150px; position: relative; border-radius: 10px; top: 5%; left: 20%;">No</button>
+        <div style="justify-self: center; justify-content: center; font-size: 20px; font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif; display: flex;  position: relative; gap: 30px;">
+            <button style="width: 150px; background: black; color: white; height: 100px; position: relative; border-radius: 10px; top: 200px;">Yes</button>
+            <br>
+            <button style="width: 150px; background: black; color: white; height: 100px; position: relative; border-radius: 10px; top: 200px;">No</button>
         </div>
     </div>
 </body>
